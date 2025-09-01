284f2b7ded9ed57e332787f46461df29e3bb47df
pandiarajs2000 <pandiarajs2000@gmail.com>
2025-09-01 22:56:45 +0530

    update the create_new_item()

--- Test/test_stock_management.py
+++ Test/test_stock_management.py
@@ -5,19 +5,36 @@ import pytest
 import time
 import allure
 import logging
+import pandas as pd
+
 
 @allure.description("To open a stock management page")
 @allure.title("To open a stock management page")
 @allure.testcase("TC-0001")
-@pytest.mark.parametrize("useremail, password, filter, equal, value", [('pandiarajs2000@gmail.com', 'Test@123', 'Disabled','Equals','Yes')])
-def test_sidemenu_window_title(driver, useremail, password,filter,equal,value):
+@pytest.mark.parametrize("useremail, password", [('pandiarajs2000@gmail.com', 'Test@123')])
+def test_sidemenu_window_title(driver, useremail, password):
     logging.info("To get the sidemenu options list")
     login_form = LoginClass(driver)
     login_form.login_form()
     login_form.login_form_field(useremail, password)
     login_form.alert_window_close()
     stock_page = StockManagement(driver)
-    response = stock_page.stock_page_access(filter,equal,value)
+    response = stock_page.stock_page_access()
     for i in str(response):
         print("Filter Data", i)
     time.sleep(3)
+
+@allure.description("To create an New Item")
+@allure.title("To open a stock management page")
+@allure.testcase("TC-0001")
+@pytest.mark.parametrize("useremail, password", [('pandiarajs2000@gmail.com', 'Test@123')])
+def test_create_new_item(driver, useremail, password, excel_sheet):
+    logging.info("To get the sidemenu options list")
+    login_form = LoginClass(driver)
+    login_form.login_form()
+    login_form.login_form_field(useremail, password)
+    login_form.alert_window_close()
+    stock_page = StockManagement(driver)
+    stock_page.stock_page_access()
+    data_sheet = excel_sheet
+    
