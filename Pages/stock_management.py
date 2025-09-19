1f644bf8584a00c4a378dc3e847aedbcbdec1491
pandiarajs2000 <pandiarajs2000@gmail.com>
2025-09-19 23:28:09 +0530

    update the xpath for the screenshots

--- Pages/stock_management.py
+++ Pages/stock_management.py
@@ -59,7 +59,7 @@ class StockManagement:
         self.auto_reorder_row_edit = (By.XPATH, "//div[@data-fieldname='__column_9']/descendant::div[@class='col']")
         self.reorder_checkin_warehouse = (By.XPATH,"//div[@class='form-group']/child::div[@class='control-input-wrapper']//div[@class='control-input']//input[contains(@data-fieldname,'warehouse_group')]")
         self.order_warehouse_option = (By.XPATH,"//div[@class='awesomplete']/child::ul[@id='awesomplete_list_23']//div[@role='option']//p")
-        self.reorder_request_for = (By.XPATH, "//div[@class='form-group']/child::div[@class='control-input-wrapper']//div[@class='control-input']//input[contains(@data-fieldname,'warehouse') and contains(@aria-owns,'awesomplete_list_24')]")
+        self.reorder_request_for = (By.XPATH, "//div[@data-fieldname='warehouse']//input[@role='combobox']")
         self.reorder_request_for_option = (By.XPATH,"//div[@class='awesomplete']/child::ul[@id='awesomplete_list_24']//div[@role='option']//p")
         self.reorder_level = (By.XPATH, "//div[@class='form-group']/child::div[@class='control-input-wrapper']//div[@class='control-input']//input[contains(@data-fieldname,'warehouse_reorder_level')]")
         self.reorder_warehouse_qty = (By.XPATH,"//div[@class='form-group']/child::div[@class='control-input-wrapper']//div[@class='control-input']//input[contains(@data-fieldname,'warehouse_reorder_qty')]")
@@ -71,6 +71,8 @@ class StockManagement:
 
         # self.save_btn_xpath = (By.XPATH, "//div[@class='container']/child::div[contains(@class, 'row') and contains(@class, 'flex') and contains(@class, 'align-center') and contains(@class, 'justify-between') and contains(@class, 'page-head-content')]/child::div[contains(@class, 'col') and contains(@class, 'flex') and contains(@class, 'justify-content-end') and contains(@class, 'page-actions')]/descendant::button[contains(@data-label, 'Save')]")
         self.save_btn_xpath = (By.XPATH, "//div[@id='page-Item']//button[@data-label='Save']")
+
+        self.check_item_enable = (By.XPATH, "//span[@class='indicator-pill no-indicator-dot whitespace-nowrap blue']//span[contains(text(),'Enabled')]")
     
     def stock_page_access(self):
         WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(self.stock_link_xpath))
@@ -89,7 +91,7 @@ class StockManagement:
         move_to_ele.click()
         time.sleep(2)
 
-    def add_item(self, excel_sheet_path, sheet_name):
+    def add_item(self, excel_sheet_path, sheet_name, row):
         add_item = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.item_add_btn))
         self.driver.get_screenshot_as_file("E:/Erpnext Automation/Screenshots/add_item_btn_click.png")
         add_item.click()
@@ -108,12 +110,13 @@ class StockManagement:
 
         full_form_open = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.item_add_full_form))
         full_form_open.click()
-        item_datas = get_item_master_datas(excel_sheet_path, sheet_name)
+        item_datas = get_item_master_datas(excel_sheet_path, sheet_name, row)
         print("Item Datas", item_datas)
         for item in item_datas:
             try:
-                item_code = self.driver.find_element(*self.item_code__two_xpath)
-                item_code.send_keys(item['code'])
+                if item['code']:
+                    item_code = self.driver.find_element(*self.item_code__two_xpath)
+                    item_code.send_keys(item['code'])
                 # item_group = self.driver.find_element(*self.item_group_xpath)
                 item_group = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.item_group_xpath))
                 item_group.clear()
@@ -200,28 +203,29 @@ class StockManagement:
                 weight_uom.send_keys(item['weight_uom'])
                 weight_uom.send_keys(Keys.ENTER)
 
-                barcode_add = WebDriverWait(self.driver,5).until(EC.element_to_be_clickable(self.barcode_row_add_btn))
-                barcode_add.click()
-                self.driver.get_screenshot_as_file("E:/Erpnext Automation/Screenshots/barcode_add_empty.png")
+                if item['barcode_sn'] and item['barcode_type'] and item['barcode_uom']:
+                    barcode_add = WebDriverWait(self.driver,5).until(EC.element_to_be_clickable(self.barcode_row_add_btn))
+                    barcode_add.click()
+                    self.driver.get_screenshot_as_file("E:/Erpnext Automation/Screenshots/barcode_add_empty.png")
 
-                WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.barcode_popup))
-                time.sleep(0.4)
+                    WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.barcode_popup))
+                    time.sleep(0.4)
 
-                barcode_num = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.barcode_sn))
-                barcode_num.clear()
-                barcode_num.send_keys(item['barcode_sn'])
+                    barcode_num = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.barcode_sn))
+                    barcode_num.clear()
+                    barcode_num.send_keys(item['barcode_sn'])
 
-                barcode_sn_type = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.barcode_type))
-                select_barcode_type = Select(barcode_sn_type)
-                select_barcode_type.select_by_value(item['barcode_type'])
+                    barcode_sn_type = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.barcode_type))
+                    select_barcode_type = Select(barcode_sn_type)
+                    select_barcode_type.select_by_value(item['barcode_type'])
 
-                barcode_uoms = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.barcode_uom))
-                barcode_uoms.clear()
-                barcode_uoms.send_keys(item['barcode_uom'])
-                barcode_uoms.send_keys(Keys.ENTER)
+                    barcode_uoms = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.barcode_uom))
+                    barcode_uoms.clear()
+                    barcode_uoms.send_keys(item['barcode_uom'])
+                    barcode_uoms.send_keys(Keys.ENTER)
 
-                close_child_barcode= WebDriverWait(self.driver,5).until(EC.element_to_be_clickable(self.close_child_tbl_row))
-                close_child_barcode.click()
+                    close_child_barcode= WebDriverWait(self.driver,5).until(EC.element_to_be_clickable(self.close_child_tbl_row))
+                    close_child_barcode.click()
 
                 open_auto_reorder = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.auto_reorder_collapse_click))
                 open_auto_reorder.click()
@@ -236,15 +240,15 @@ class StockManagement:
                 edit_row.click()
                 self.driver.get_screenshot_as_file("E:/Erpnext Automation/Screenshots/auto_reorder_edit_row.png")
 
-                check_in_group = WebDriverWait(self.driver,5).until(EC.presence_of_element_located(self.reorder_checkin_warehouse))
+                check_in_group = WebDriverWait(self.driver,10).until(EC.presence_of_element_located(self.reorder_checkin_warehouse))
                 check_in_group.clear()
                 check_in_group.send_keys(item['checkin_warehouse'])
                 check_in_group.send_keys(Keys.ARROW_DOWN)
                 time.sleep(0.5)
                 check_in_group.send_keys(Keys.ENTER)
-                time.sleep(2)
+                # time.sleep(2)
 
-                request_for_wareshouse = WebDriverWait(self.driver,5).until(EC.presence_of_element_located(self.reorder_request_for))
+                request_for_wareshouse = WebDriverWait(self.driver,10).until(EC.presence_of_element_located(self.reorder_request_for))
                 request_for_wareshouse.clear()
                 request_for_wareshouse.send_keys(item['request_for_wh'])
                 request_for_wareshouse.send_keys(Keys.ARROW_DOWN)
@@ -252,7 +256,7 @@ class StockManagement:
                 request_for_wareshouse.send_keys(Keys.ENTER)
                 time.sleep(2)
 
-                re_order_level_reach = WebDriverWait(self.driver,5).until(EC.presence_of_element_located(self.reorder_level))
+                re_order_level_reach = WebDriverWait(self.driver,10).until(EC.presence_of_element_located(self.reorder_level))
                 re_order_level_reach.clear()
                 re_order_level_reach.send_keys(item['reorder_level'])
 
@@ -260,18 +264,22 @@ class StockManagement:
                 re_order_qty.clear()
                 re_order_qty.send_keys(item['reorder_qty'])
 
-                material_type = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.reorder_purpose))
+                material_type = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.reorder_purpose))
                 material_type_select = Select(material_type)
                 material_type_select.select_by_value(item['mr_type'])
 
                 close_auto_reoder_table = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.auto_reorder_tbl_close))
                 close_auto_reoder_table.click()
+                time.sleep(2)
 
                 ActionChains(self.driver).scroll_by_amount(0,-50).perform()
 
                 save_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.save_btn_xpath))
                 save_btn.click()
                 time.sleep(0.5)
+
+                error_msg = None
+                item_saved_msg = None
                 self.driver.get_screenshot_as_file("E:/Erpnext Automation/Screenshots/barcode_error.png")
                 try:
                     barcode_error = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.barcode_error_msg))
@@ -280,13 +288,17 @@ class StockManagement:
                     time.sleep(5)
                 except Exception as e:
                     pass
+
                 try:
-                    success_message = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.success_msg))
-                    item_saved_msg = success_message.text
+                    enable_text=WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.check_item_enable))
+                    # alert_found = self.driver.switch_to.alert
+                    success = enable_text.text
+                    print("Saved =>", success)
                     time.sleep(5)
+                    self.driver.get_screenshot_as_file("E:/Erpnext Automation/Screenshots/item_added.png")
                 except Exception as e:
                     pass
-                return {"error": error_msg, "success": item_saved_msg}
+                return {"error": error_msg, "success": success}
             except Exception as e:
                 print("Exception Type", type(e))
                 print("Exception Name:", e.__class__.__name__)
@@ -295,12 +307,12 @@ class StockManagement:
                     print(f"File: {frame.filename}, Line: {frame.lineno}, Code: {frame.line}")
         time.sleep(5)
 
-def get_item_master_datas(excel_file_path, sheet_name):
+def get_item_master_datas(excel_file_path, sheet_name, start_row):
     workbook = openpyxl.load_workbook(excel_file_path)
     sheet = workbook[sheet_name]
 
     item_data = []
-    for i in range(2, sheet.max_row+1):
+    for i in range(start_row, sheet.max_row+1):
         item_ = {
             "code" : sheet.cell(row=i, column=5).value,
             "item_name" : sheet.cell(row=i, column=6).value,
